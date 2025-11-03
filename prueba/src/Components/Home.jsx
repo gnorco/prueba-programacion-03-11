50320c8e7d46b43bb571f62afce0503ac958ebf2
gnorco <gnardironco@escuelasproa.edu.ar>
2025-11-03 10:15:22 -0300

    segundo commit

---
+++ prueba/src/Components/Home.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const ObtenerDatos = async () => {
+      try {
+        const res = await axios.get("https://dummyjson.com/users");
+        setData(res.data.users.slice(0, 5));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    ObtenerDatos();
+  }, []);
+
+  return (
+    <div>
+      <h1>Usuarios</h1>
+
+      {data.map((user) => (
+        <div key={user.id} style={{ marginBottom: "20px" }}>
+          <h3>
+            {user.firstName} {user.lastName}
+          </h3>
+          <p>Ciudad: {user.address.city}</p>
+
+          <Link to={`/user/${user.id}`}>
+            <button>Ver detalles</button>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
